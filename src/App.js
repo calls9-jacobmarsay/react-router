6075a0f061b02727c6bd073089af8b4f1e5450db
Jacob Marsay <jacobmarsay@calls9.com>
2022-12-15 12:39:45 +0000

    Added styles

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./components/pages/Welcome";
 import Products from "./components/pages/Products";
 import MainHeader from "./components/MainHeader";
+
 function App() {
   return (
     <div>
       <MainHeader />
-      <Routes>
-        <Route path="/welcome" element={<Welcome />}></Route>
+      <main>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />}></Route>
 
-        <Route path="/products" element={<Products />}></Route>
-      </Routes>
+          <Route path="/products" element={<Products />}></Route>
+        </Routes>
+      </main>
     </div>
   );
 }
