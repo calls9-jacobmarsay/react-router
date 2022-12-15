8e44615c77ea371a5c3d5c48d7f65aac399bcb11
Jacob Marsay <jacobmarsay@calls9.com>
2022-12-15 14:31:51 +0000

    Extracted product id using params

--- src/components/pages/ProductDetails.js
+++ src/components/pages/ProductDetails.js
@@ -1,8 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetails = () => {
+  const params = useParams();
   return (
     <section>
       <h1>Product Title</h1>
       <p>Product description</p>
+      <p>{params.productId}</p>
     </section>
   );
 };
